8684d5a1a87c0e882c09e5f7b19f00d4a55a7ca2
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-12-08 14:18:17 -0800

    refactoring

--- src/Components/DetailPage.tsx
+++ src/Components/DetailPage.tsx
@@ -19,7 +19,7 @@ const DetailPage = (props: Props) => {
     <>
       <div className='absolute top-0 right-0 left-0 bottom-0 opacity-80 bg-gray-500' onClick={() => setShowDetailPage(false)}>
       </div>
-      <div className={`absolute top-24 right-0 left-0 md:w-[480px] w-[300px] h-[520px] mx-auto opacity-100 rounded-2xl p-4 ${theme === 'dark' ? 'text-white bg-gray-700' : 'text-black bg-amber-50'}`} >
+      <div className={`absolute top-16 md:top-24 z-20 right-0 left-0 md:w-[480px] w-[300px] h-[480px] mx-auto opacity-100 rounded-2xl p-4 ${theme === 'dark' ? 'text-white bg-gray-700' : 'text-black bg-amber-50'}`} >
         <img src={`https://image.tmdb.org/t/p/w500/${detailImage}`} alt="dd" className='w-auto h-[60%] object-cover block mx-auto rounded-2xl' />
         <div className='h-[40%] overflow-y-scroll mt-4'>
           <h3 className='h-auto mb-2 text-center text-3xl'>{detailTitle}</h3>
